ae28eda5e10dd2bf116f682033d4800c76b39b3c
Haritha2498 <haritha2015a@gmail.com>
2024-09-02 14:51:25 +0530

    main

--- src/components/Third.jsx
+++ src/components/Third.jsx
@@ -6,7 +6,8 @@ const Third = () => {
     <div className='mx-auto'>
             <p  className="text-xl">I am<b> Haritha A Rajan</b>.I am a <i>BE Computer Science and Engineering Graduate</i>.I completed my graduation from Anna University,Chennai.
                     <br /><br />Currently I am doing Blockchain Architect program from Digital University Kerala,Trivandrum.I am native to 
-                    Trivandrum,Kerala,India.<br />I had also completed an Internship training on Basic Telecommunications from BSNL.Also had an Internship training on Web Technologies.<br />  
+                    Trivandrum,Kerala,India.<br />I had completed an Internship training on Basic Telecommunications .Also had an Internship training on Web Technologies.<br /> 
+                    <br>I had completed some Certification provided by Kerala Blockchain Academy</br> 
                     <br />
                     <br />
             </p></div>
